60861cd52864d4bcb51b5af1379aa1a8c56b9c4d
Anwarlove <doanthien54@gmail.com>
2024-10-31 23:24:28 +0700

    background Member

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar/Navbar";
 import Wild from "./components/Wild/Wild";
 import Product from "./components/Product/Product";
 import RareAnimals from "./components/RareAnimals/RareAnimals";
+import Premium from "./components/Premium/Premium";
+import Saleproduct from "./components/SaleProduct/Saleproduct";
 import AOS from "aos";
 import "aos/dist/aos.css";
 const App = () => {
@@ -21,6 +23,8 @@ const App = () => {
       <Wild />
       <Product />
       <RareAnimals />
+      <Premium />
+      <Saleproduct />
     </div>
   );
 };
---
+++ src/components/Premium/Premium.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import classNames from "classnames";
+import backg from "../../assets/member/Doggie.png";
+
+const Premium = () => {
+  return (
+    <div
+      className={classNames(`block text-center mt-28 bg-yellow-600`)}
+      style={{
+        backgroundImage: `url(${backg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="space-y-5 container">
+        <div className="text-3xl font-bold text-white pt-14">
+          <h1>Get Notified About Latest Updates On Your Benefits</h1>
+        </div>
+        <div>
+          <input
+            className="w-full p-3 max-w-2xl rounded-sm mt-9 outline-gray-400"
+            type="text"
+            placeholder="Enter your email"
+          />
+        </div>
+        <div>
+          <input
+            className="w-full p-3 max-w-2xl rounded-sm outline-gray-400"
+            type="text"
+            placeholder="Enter your phone"
+          />
+        </div>
+        <button className="p-3 bg-secondary rounded-full text-white font-bold">
+          Continue
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Premium;
